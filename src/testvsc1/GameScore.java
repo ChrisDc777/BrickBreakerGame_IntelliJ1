022cf0b47ca45eceb7354a0d7ac7c560b836b926
ChrisDc777 <chris.dcosta.btech2021@sitpune.edu.in>
2023-04-19 00:37:20 +0530

    Applied unique key for name column and added UPDATE command to update player's score if new score is higher.

--- src/testvsc1/GameScore.java
+++ src/testvsc1/GameScore.java
@@ -20,99 +20,21 @@ public class GameScore {
     To store high scores in an ArrayList and display them
      */
 
-//    public void updateScores(Graphics g, String playerName, int score) {
-//        try (Connection con = dbConnection.getConnection()) {
-//            Statement stmt = con.createStatement();
-//
-//            String sql = "INSERT INTO scores (name, score) VALUES ('" + playerName + "', " + score + ") ON DUPLICATE KEY UPDATE score = GREATEST(score," + score + ")";
-//            stmt.executeUpdate(sql);
-//
-//            String sql1 = "SELECT name, score FROM scores ORDER BY score DESC LIMIT 10";
-//            ResultSet rs = stmt.executeQuery(sql1);
-//
-//            ArrayList<String> highScores = new ArrayList<>();
-//            while (rs.next()) {
-//                String name = rs.getString("name");
-//                int playerScore = rs.getInt("score");
-//                highScores.add(name + ": " + playerScore);
-//            }
-//
-//            g.setFont(new Font("MV Boli", Font.BOLD, 20));
-//            g.drawString("High Scores", 300, 350);
-//            int y = 380;
-//            for (String highScore : highScores) {
-//                g.drawString(highScore, 300, y);
-//                y += 30;
-//            }
-//
-//            // Check if player's score is the highest
-//            String sql2 = "SELECT MAX(score) AS highscore FROM scores";
-//            ResultSet rs2 = stmt.executeQuery(sql2);
-//            rs2.next();
-//            int highScore = rs2.getInt("highscore");
-//            if (score == highScore) {
-//                g.setFont(new Font("Arial", Font.BOLD, 30));
-//                g.setColor(Color.RED);
-//                g.drawString("Congratulations, you have the highest score!", 50, 80);
-//            }
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
-
-
-//    public void updateScores(Graphics g, String playerName, int score) {
-//        try (Connection con = dbConnection.getConnection()) {
-//            Statement stmt = con.createStatement();
-//
-//            String sql = "INSERT INTO scores (name, score) VALUES ('" + playerName + "', " + score + ") ON DUPLICATE KEY UPDATE score = GREATEST(score," + score + ")";
-//            stmt.executeUpdate(sql);
-//
-//            String sql1 = "SELECT name, score FROM scores ORDER BY score DESC LIMIT 5";
-//            ResultSet rs = stmt.executeQuery(sql1);
-//
-//            ArrayList<String> highScores = new ArrayList<>();
-//            while (rs.next()) {
-//                String name = rs.getString("name");
-//                int playerScore = rs.getInt("score");
-//                highScores.add(name + ": " + playerScore);
-//            }
-//
-//            g.setFont(new Font("MV Boli", Font.BOLD, 20));
-//            g.drawString("High Scores", 300, 350);
-//            int y = 380;
-//            boolean isHighScore = false;
-//            int highestScore = 0; // add this variable to keep track of the highest score
-//            for (String highScore : highScores) {
-//                g.drawString(highScore, 300, y);
-//                y += 30;
-//                if (highScore.startsWith(playerName + ":")) {
-//                    isHighScore = true;
-//                }
-//                int colonIndex = highScore.indexOf(':');
-//                int playerScore = Integer.parseInt(highScore.substring(colonIndex + 2));
-//                if (playerScore > highestScore) { // update the highest score
-//                    highestScore = playerScore;
-//                }
-//            }
-//            if (isHighScore && score == highestScore) { // display the congratulations message only if the player's score is the highest score
-//                g.drawString("Congratulations, " + playerName + "! You have the highest score!", 150, 450);
-//            }
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
-
-
     public void updateScores(Graphics g, String playerName, int score) {
         try (Connection con = dbConnection.getConnection()) {
             Statement stmt = con.createStatement();
 
-            String sql = "INSERT INTO scores (name, score) VALUES ('" + playerName + "', " + score + ") ON DUPLICATE KEY UPDATE score = GREATEST(score," + score + ")";
-            stmt.executeUpdate(sql);
+            // Update player's high score if new score is higher
+            String updateSql = "UPDATE scores SET score = " + score + " WHERE name = '" + playerName + "' AND " + score + " > score";
+            stmt.executeUpdate(updateSql);
 
-            String sql1 = "SELECT name, score FROM scores ORDER BY score DESC LIMIT 10";
-            ResultSet rs = stmt.executeQuery(sql1);
+            // Insert new score or update existing score
+            String insertSql = "INSERT INTO scores (name, score) VALUES ('" + playerName + "', " + score + ") ON DUPLICATE KEY UPDATE score = GREATEST(score," + score + ")";
+            stmt.executeUpdate(insertSql);
+
+            // Retrieve top 10 high scores
+            String selectSql = "SELECT name, score FROM scores ORDER BY score DESC LIMIT 10";
+            ResultSet rs = stmt.executeQuery(selectSql);
 
             ArrayList<String> highScores = new ArrayList<>();
             while (rs.next()) {
@@ -132,4 +54,5 @@ public class GameScore {
             System.out.println(e.getMessage());
         }
     }
+
 }
